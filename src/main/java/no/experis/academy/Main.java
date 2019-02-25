6100b71938af9ba30a592ebd13f2d477c2aabefe
Thinh To <thinhtm.95@gmail.com>
2019-02-25 15:36:24 +0100

    added postgres

--- src/main/java/no/experis/academy/Main.java
+++ src/main/java/no/experis/academy/Main.java
@@ -1,24 +1,7 @@
 package no.experis.academy;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args){
-        connect();
-    }
 
-    public static Connection connect() {
-        Connection conn = null;
-        String url = "jdbc:postgresql://localhost:5432/local?user=postgres";
-
-        try {
-            conn = DriverManager.getConnection(url);
-            System.out.println("Connection established");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return conn;
     }
 }
--- src/main/java/no/experis/academy/SqlHelper/PersonQuery.java
+++ src/main/java/no/experis/academy/SqlHelper/PersonQuery.java
@@ -2,12 +2,17 @@ package no.experis.academy.SqlHelper;
 
 import no.experis.academy.Model.Person;
 
+import java.sql.Connection;
+
 public class PersonQuery {
     private PersonQuery() {
 
     }
 
     public static Person getPerson(int id) {
+        Connection conn = PostgresConnection.connect();
+
+
         return null;
     }
 
