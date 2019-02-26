62c04bcd0fbfbdb522c7a8956378ae2f4e97f922
Billy Infante <infbil15@gmail.com>
2019-02-26 13:43:45 +0100

    hi

---
+++ src/main/java/no/experis/academy/SqlHelper/EmailQuery.java
@@ -0,0 +1,32 @@
+package no.experis.academy.SqlHelper;
+
+import no.experis.academy.Interfaces.CRUD;
+import no.experis.academy.Model.Email;
+
+public class EmailQuery implements CRUD<Email> {
+
+    @Override
+    public Iterable<Email> getAll() {
+        return null;
+    }
+
+    @Override
+    public Email getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(Email item) {
+
+    }
+
+    @Override
+    public void update(Email item) {
+
+    }
+
+    @Override
+    public Email delete(Email item) {
+        return null;
+    }
+}
