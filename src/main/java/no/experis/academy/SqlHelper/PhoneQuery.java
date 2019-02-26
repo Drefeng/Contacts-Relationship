c63fb0947dbe12ea5509e101e44b51b2ca25feaf
Billy Infante <infbil15@gmail.com>
2019-02-26 13:41:19 +0100

    Hello

--- src/main/java/no/experis/academy/SqlHelper/PhoneQuery.java
+++ src/main/java/no/experis/academy/SqlHelper/PhoneQuery.java
@@ -1,4 +1,32 @@
 package no.experis.academy.SqlHelper;
 
-public class PhoneQuery {
+import no.experis.academy.Interfaces.CRUD;
+import no.experis.academy.Model.Phonenumber;
+
+public class PhoneQuery implements CRUD<Phonenumber> {
+
+    @Override
+    public Iterable<Phonenumber> getAll() {
+        return null;
+    }
+
+    @Override
+    public Phonenumber getById(int id) {
+        return null;
+    }
+
+    @Override
+    public void add(Phonenumber item) {
+
+    }
+
+    @Override
+    public void update(Phonenumber item) {
+
+    }
+
+    @Override
+    public Phonenumber delete(Phonenumber item) {
+        return null;
+    }
 }
