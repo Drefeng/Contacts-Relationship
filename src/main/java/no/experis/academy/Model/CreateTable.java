260aa9dfc179046918e7a1a2a11023f2948a75b5
fajardoralf <fajardoralf@gmail.com>
2019-02-26 11:07:39 +0100

    fixed create tables

--- src/main/java/no/experis/academy/Model/CreateTable.java
+++ src/main/java/no/experis/academy/Model/CreateTable.java
@@ -3,6 +3,7 @@ package no.experis.academy.Model;
 import no.experis.academy.SqlHelper.PostgresConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,7 +15,7 @@ public class CreateTable {
         try{
             connect = PostgresConnection.connect();
             stmt = connect.createStatement();
-            String sql = "CREATE TABLE person"
+            String sql = "CREATE TABLE IF NOT EXISTS persons"
                         + "(id SERIAL PRIMARY KEY NOT NULL,"
                         + "firstname VARCHAR(100) NOT NULL,"
                         + "lastname VARCHAR(100) NOT NULL,"
@@ -27,7 +28,7 @@ public class CreateTable {
             System.out.println(e.getMessage());
             System.exit(0);
         }
-        System.out.println("Person table created successfully");
+        System.out.println("Persons table created successfully");
     }
 
     public static void createPhoneTable(){
@@ -36,12 +37,12 @@ public class CreateTable {
         try{
             connect = PostgresConnection.connect();
             stmt = connect.createStatement();
-            String sql = "CREATE TABLE phone"
+            String sql = "CREATE TABLE IF NOT EXISTS phones"
                     + "(id SERIAL PRIMARY KEY NOT NULL,"
                     + "home INT ,"
                     + "mobile INT,"
                     + "work INT,"
-                    + "phoneref INT REFERENCES person (id));";
+                    + "phoneref INT REFERENCES persons (id));";
 
             stmt.executeUpdate(sql);
             stmt.close();
@@ -59,11 +60,11 @@ public class CreateTable {
         try{
             connect = PostgresConnection.connect();
             stmt = connect.createStatement();
-            String sql = "CREATE TABLE email"
+            String sql = "CREATE TABLE IF NOT EXISTS emails"
                     + "(id SERIAL PRIMARY KEY NOT NULL,"
                     + "personal VARCHAR(100),"
                     + "work VARCHAR(100),"
-                    + "emailref INT REFERENCES person (id));";
+                    + "emailref INT REFERENCES persons (id));";
 
             stmt.executeUpdate(sql);
             stmt.close();
@@ -81,11 +82,12 @@ public class CreateTable {
         try{
             connect = PostgresConnection.connect();
             stmt = connect.createStatement();
-            String sql = "CREATE TABLE address"
+            String sql = "CREATE TABLE IF NOT EXISTS addresses"
                     + "(id SERIAL PRIMARY KEY NOT NULL,"
                     + "current VARCHAR(100),"
                     + "work VARCHAR(100),"
-                    + "address_ref INT REFERENCES person (id));";
+                    + "address_ref INT REFERENCES persons (id));";
+
 
             stmt.executeUpdate(sql);
             stmt.close();
@@ -97,24 +99,35 @@ public class CreateTable {
         System.out.println("Address table created successfully");
     }
 
-    public static void createRelationshipTable(){
-        Connection connect = null;
-        Statement stmt = null;
-        try{
-            connect = PostgresConnection.connect();
-            stmt = connect.createStatement();
-            String sql = "CREATE TABLE relationship"
-                    + "(id SERIAL PRIMARY KEY NOT NULL,"
-                    + "role VARCHAR(100));";
+    public static void createRelationshipTable() throws Exception{
+        Connection connect = PostgresConnection.connect();
+        Statement stmt = connect.createStatement();
 
-            stmt.executeUpdate(sql);
-            stmt.close();
-            connect.close();
-        }catch(Exception e){
-            System.out.println(e.getMessage());
-            System.exit(0);
+        String createTable = "CREATE TABLE IF NOT EXISTS relationships"
+                + "(id SERIAL PRIMARY KEY NOT NULL,"
+                + "role VARCHAR(100) UNIQUE);";
+        PreparedStatement create = connect.prepareStatement(createTable);
+
+
+        String updateTable = "INSERT INTO relationships"
+                + "(role)"
+                + "VALUES"
+                + "('Father'), ('Mother'), ('Brother'), ('Sister')";
+        PreparedStatement update = connect.prepareStatement(updateTable);
+
+        boolean autoCommit = connect.getAutoCommit();
+        try{
+            connect.setAutoCommit(false);
+            create.executeUpdate();
+            update.executeUpdate();
+            connect.commit();
+            System.out.println("Relationship table created successfully");
+        }catch(SQLException e){
+            connect.rollback();
+        }finally {
+            connect.setAutoCommit(autoCommit);
         }
-        System.out.println("Relationship table created successfully");
+
     }
 
     public static void createFamilyTable(){
@@ -123,11 +136,11 @@ public class CreateTable {
         try{
             connect = PostgresConnection.connect();
             stmt = connect.createStatement();
-            String sql = "CREATE TABLE family"
+            String sql = "CREATE TABLE IF NOT EXISTS familys"
                     + "(id SERIAL PRIMARY KEY NOT NULL,"
-                    + "person_id INT REFERENCES person (id) NOT NULL,"+
-                    " relative_id INT REFERENCES person (id) NOT NULL,"
-                    + "relation_id INT REFERENCES relationship (id));";
+                    + "person_id INT REFERENCES persons (id) NOT NULL,"+
+                    " relative_id INT REFERENCES persons (id) NOT NULL,"
+                    + "relation_id INT REFERENCES relationships (id));";
 
             stmt.executeUpdate(sql);
             stmt.close();
@@ -140,11 +153,16 @@ public class CreateTable {
     }
 
     public static void createTables() {
-        createPersonTable();
-        createAddressTable();
-        createPhoneTable();
-        createEmailTable();
-        createRelationshipTable();
-    }
+        try{
+            createPersonTable();
+            createAddressTable();
+            createPhoneTable();
+            createEmailTable();
+            createRelationshipTable();
+            createFamilyTable();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
 
+    }
 }
