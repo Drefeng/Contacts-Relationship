5653394d656f1a7632908d634437305b9c050511
fajardoralf <fajardoralf@gmail.com>
2019-02-26 12:33:12 +0100

    checked persons methods

--- src/main/java/no/experis/academy/Main.java
+++ src/main/java/no/experis/academy/Main.java
@@ -2,6 +2,7 @@ package no.experis.academy;
 
 import no.experis.academy.Model.CreateTable;
 import no.experis.academy.Model.Person;
+import no.experis.academy.SqlHelper.PersonQuery;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -10,6 +11,12 @@ public class Main {
     public static void main(String[] args){
         CreateTable.createTables();
         Person person1 = new Person("Ralf", "Fajardo", LocalDate.of(1988, Month.NOVEMBER, 1));
+
+        //new PersonQuery().add(person1);
+
+        person1 = new PersonQuery().getById(1);
+
+        System.out.println(person1.getId());
 
 
     }
