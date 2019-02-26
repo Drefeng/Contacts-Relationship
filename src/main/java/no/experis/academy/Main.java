06411363d51e2173c55335bb248ddf18e1a16c05
fajardoralf <fajardoralf@gmail.com>
2019-02-26 12:17:50 +0100

    hot fix address

--- src/main/java/no/experis/academy/Main.java
+++ src/main/java/no/experis/academy/Main.java
@@ -1,7 +1,16 @@
 package no.experis.academy;
 
+import no.experis.academy.Model.CreateTable;
+import no.experis.academy.Model.Person;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args){
-        
+        CreateTable.createTables();
+        Person person1 = new Person("Ralf", "Fajardo", LocalDate.of(1988, Month.NOVEMBER, 1));
+
+
     }
 }
