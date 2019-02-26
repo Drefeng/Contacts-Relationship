5b42598fea651e6e9e8a61e7d7a41f30e281644c
Billy Infante <infbil15@gmail.com>
2019-02-26 12:13:26 +0100

    add familyQueries

--- src/main/java/no/experis/academy/Main.java
+++ src/main/java/no/experis/academy/Main.java
@@ -1,5 +1,6 @@
 package no.experis.academy;
 
+import no.experis.academy.Model.CreateTable;
 import no.experis.academy.Model.Person;
 import no.experis.academy.SqlHelper.PersonQuery;
 
@@ -8,13 +9,16 @@ import java.util.Date;
 import java.util.Iterator;
 
 public class Main {
-    public static void main(String[] args){
-        Iterator it = new PersonQuery().getAll().iterator();
+    public static void main(String[] args) {
+//        Iterator it = new PersonQuery().getAll().iterator();
+//
+//        while (it.hasNext()) {
+//            Person p = (Person)it.next();
+//            System.out.printf("%s %s %s", p.getFirstName(), p.getLastName(), p.getBirthday());
+//            System.out.println();
+//        }
+//
 
-        while (it.hasNext()) {
-            Person p = (Person)it.next();
-            System.out.printf("%s %s %s", p.getFirstName(), p.getLastName(), p.getBirthday());
-            System.out.println();
-        }
+
     }
 }
--- src/main/java/no/experis/academy/Model/Family.java
+++ src/main/java/no/experis/academy/Model/Family.java
@@ -2,18 +2,25 @@ package no.experis.academy.Model;
 
 public class Family {
 
+    private int id;
     private int p_id;
     private int rel_id;
     private int relation_id;
 
-
-    public Family(int p_id, int rel_id, int relation_id) {
+    public Family(int id, int p_id, int rel_id, int relation_id) {
+        this.id = id;
         this.p_id = p_id;
         this.rel_id = rel_id;
         this.relation_id = relation_id;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
     public int getP_id() {
         return p_id;
     }
